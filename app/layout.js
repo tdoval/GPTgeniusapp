bab3b5969ef4b8d6327fb6d526dd10eaca207065
tdoval <thiagodovalaraujo@gmail.com>
2024-02-03 14:33:29 -0300

    sidebar navigation

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
+
+
 export const metadata = {
   title: "GPTGenius",
   description: "GPTGenius: Your AI language companion. Powered by OpenAI, it enhances your conversations, content creation, and more!",
---
+++ components/ThemeToggle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ThemeToggle = () => {
+    return (
+        <button className='btn btn-primary btn-sm'>ThemeToggle</button>
+    )
+}
+
+export default ThemeToggle

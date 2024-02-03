25990dac3de5872df29a5902805167cb541354d6
tdoval <thiagodovalaraujo@gmail.com>
2024-02-03 16:03:23 -0300

    @feature/Theme toggle

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { UserButton, auth, currentUser } from '@clerk/nextjs'
 
-const MemberProfile = () => {
+const MemberProfile = async () => {
+    const user = await currentUser()
+    const { userId } = auth();
     return (
-        <div>MemberProfile</div>
+        <div className='px-4 flex items-center gap-2'>
+            <UserButton afterSignOutUrl='/' />
+            <p>{user.emailAddresses[0].emailAddress}</p>
+        </div>
     )
 }
 

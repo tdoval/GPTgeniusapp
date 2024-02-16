2413b0b3f8e4ae917ea04ddf18ff661829f51b22
tdoval <thiagodovalaraujo@gmail.com>
2024-02-16 11:41:27 -0300

    @feature/UnspashAPI

--- app/(dashboard)/tours/[id]/page.js
+++ app/(dashboard)/tours/[id]/page.js
@@ -1,7 +1,10 @@
 import TourInfo from '@/components/TourInfo';
-import { getSingleTour } from '@/utils/actions'
+import { generateTourImage, getSingleTour } from '@/utils/actions'
 import { redirect } from 'next/dist/server/api-utils'
+import Image from 'next/image';
 import Link from 'next/link';
+import axios from 'axios';
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=`;
 
 const SingleTourPage = async ({ params }) => {
     const tour = await getSingleTour(params.id)
@@ -9,12 +12,33 @@ const SingleTourPage = async ({ params }) => {
     if (!tour) {
         redirect('/tours');
     }
+    const { data } = await axios.get(`${url}${tour.city}`);
+    const tourImage = data?.results[0]?.urls?.raw;
+    /**
+     * OPEN AI IMAGE GENERATE
+     */
+    // const tourImage = await generateTourImage({
+    //     city: tour.city,
+    //     country: tour.country
+    // });
 
     return (
         <div>
             <Link href='/tours' className='btn btn-secondary mb-12'>
                 Back to Tours
             </Link>
+            {
+                tourImage ? <div>
+                    <Image
+                        src={tourImage}
+                        width={300}
+                        height={300}
+                        className='rounded-xl shadow-xl mb-16 h-96 w-96 object-cover'
+                        alt={tour.title}
+                        priority
+                    />
+                </div> : null
+            }
             <TourInfo tour={tour} />
         </div>
     )
--- utils/actions.js
+++ utils/actions.js
@@ -119,3 +119,17 @@ export const getSingleTour = async (id) => {
         },
     });
 };
+
+export const generateTourImage = async ({ city, country }) => {
+    try {
+        const tourImage = await openai.images.generate({
+            prompt: `a panoramic view of the ${city}, in the ${country}`,
+            n: 1,
+            size: '512x512'
+        })
+        return tourImage?.data[0]?.url
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
